9cd18443059de5ed89b0e6bb7cdbec6b4150d9a7
Uday Raj Singh <udayrajsingh1319>
2026-05-25 17:35:01 +0530

    fix in serverjs

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 
-await connectDB();
+
 
 const app = express();
 
@@ -31,6 +31,8 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(express.urlencoded({ extended: true }));
 
+
+//   Health check
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "OK",
@@ -69,6 +71,8 @@ app.listen(PORT, "0.0.0.0", () => {
     │ 🔗 Proxy    : Listening on all network interfaces      │
     └────────────────────────────────────────────────────────┘
   `);
+  connectDB();
 });
+
 
 
